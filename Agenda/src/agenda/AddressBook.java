4728a99f264fd06be3af33dd9f6eee7e8132374f
DrGrodus <ebermd00@gmail.com>
2021-11-20 04:15:15 -0500

    Se agregaron metodos y constructores

--- Agenda/src/agenda/AddressBook.java
+++ Agenda/src/agenda/AddressBook.java
@@ -1,6 +1,13 @@
 package agenda;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  *
@@ -8,12 +15,97 @@ import java.util.HashMap;
  */
 public class AddressBook {
     HashMap<String, String> contactos = new HashMap<>();
-    
-    public void load(){
-        
+    private String numero; private String nombre;
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
     
-    public void save(){
-        
+    public ArrayList load(String filePath){
+        BufferedReader reader = null;
+        PrintStream printS = new PrintStream(System.out);
+        ArrayList listAux = new ArrayList();
+        try{
+            List<AddressBook> listaContactos = new ArrayList<AddressBook>();
+            String line = "";
+            reader = new BufferedReader(new FileReader(filePath));
+            reader.readLine();
+            
+            while((line = reader.readLine()) != null){
+                String[] fields = line.split(",");
+                
+                if(fields.length > 0){
+                    AddressBook AB = new AddressBook();
+                    AB.setNumero(fields[0]);
+                    AB.setNombre(fields[1]);
+                    listaContactos.add(AB);
+                }
+            }
+            for(AddressBook AB: listaContactos){
+                System.out.printf("[Numero=%s, Nombre=%s]\n", AB.getNumero(), AB.getNombre());
+                printS = printS.printf("[Numero=%s, Nombre=%s]\n", AB.getNumero(), AB.getNombre());
+                listAux.add(String.valueOf(printS));
+            }
+            
+        } catch(Exception e){
+            e.printStackTrace();
+        } finally{
+            try{
+                reader.close();
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        return listAux;
     }
+    
+    public void save(String filePath){
+        List<AddressBook> listaContactos = new ArrayList<>();
+        
+        // usuarios demo 
+        AddressBook AB = new AddressBook();
+        AB.setNumero("1");
+        AB.setNombre("Jack");
+        listaContactos.add(AB);
+        
+        AB.setNumero("2");
+        AB.setNombre("Pedro");
+        listaContactos.add(AB);
+        
+        FileWriter filewriter = null;
+        try{
+            filewriter = new FileWriter(filePath);
+            filewriter.append("Numero, nombre\n");
+            for(AddressBook A: listaContactos){
+                filewriter.append(String.valueOf(A.getNumero()));
+                filewriter.append(",");
+                filewriter.append(A.getNombre());
+                filewriter.append("\n");
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        } finally{
+            try{
+                filewriter.flush();
+                filewriter.close();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+                
+    }
+    
+    
 }
--- Agenda/src/agenda/Agenda.java
+++ Agenda/src/agenda/Agenda.java
@@ -1,10 +1,90 @@
 package agenda;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  *
  * @author eber
  */
 public class Agenda {
+
+    public static AddressBook AddBo = new AddressBook();
+    private static Integer contador = 0;
+    private static ArrayList listAux = new ArrayList();
+
     public static void main(String[] args) {
         
+        String separator = FileSystems.getDefault().getSeparator();
+        String filePath = String.format(
+                "datos%sContactos.csv",
+                separator, separator, separator, separator
+        );
+        Path Pathfile = Paths.get(filePath);
+        try{
+            AddBo.save(filePath);
+            System.out.println("empezando a leer el Contactos.csv"); 
+            listAux = AddBo.load(filePath);
+            list(filePath);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
+
+    public static void list(String filePath) {
+        System.out.println(listAux);
+        
+    }
+
+    public static void create(String filePath) {
+        //List<AddressBook> listaContactos = new ArrayList<AddressBook>();
+        Scanner lector = new Scanner(System.in);
+        
+        System.out.println("Escribe el nombre");
+        String nombre = lector.nextLine();
+        int contador = 3; String contador_S = ""; 
+        contador_S = String.valueOf(contador);
+        
+        
+        AddBo.contactos.put(nombre, contador_S);
+        
+        FileWriter filewriter = null;
+        try{
+            filewriter = new FileWriter(filePath);
+            filewriter.append("Id, nombre, apellido\n");
+            //for(AddressBook u: listaContactos){
+            //    filewriter.append(String.valueOf(u.getId()));
+                filewriter.append(",");
+                //filewriter.append(u.getPrimerNombre());
+                filewriter.append(",");
+                //filewriter.append(u.getApellidos());
+                filewriter.append("\n");
+        //    }
+        } catch(IOException e){
+            e.printStackTrace();
+        } finally{
+            try{
+                filewriter.flush();
+                filewriter.close();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void delete(String filePath) {
+
+    }
+
 }
